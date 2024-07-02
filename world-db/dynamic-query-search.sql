a392d9946fa201b8723b89fd50b28b213dc520e2
devbyul <devbyul@gmail.com>
2024-07-02 10:07:33 +0900

    set varibale 삭제

--- world-db/dynamic-query-search.sql
+++ world-db/dynamic-query-search.sql
@@ -1,17 +1,11 @@
 CREATE PROCEDURE world.spSearch(
-    IN in_s_type_enum ENUM('CountryCode', 'Continent', 'Region'),
-    IN in_s_keyword_var VARCHAR(128),
+    IN in_search_type_enum ENUM('CountryCode', 'Continent', 'Region'),
+    IN in_search_keyword_var VARCHAR(128),
     IN in_order_enum ENUM('ASC','DESC'),
     IN in_limit_int INT,
     IN in_offset_int INT
 )
 BEGIN
-	SET @search_type = in_s_type_enum;
-	SET @search_keyword = in_s_keyword_var;
-	SET @order_direction = in_order_enum;
-	SET @limit = IFNULL(in_limit_int, 10);
-	SET @offset = IFNULL(in_offset_int, 0);
-
 	SET @sql_query = CONCAT("
 		SELECT 
 			city.ID,
@@ -48,25 +42,25 @@ BEGIN
 		        ) AS countrylanguage ON countrylanguage.CountryCode = city.CountryCode"
 	);
 
-    # WHERE - Search Tabel  `country` 
-    #         Search Column `CountryCode`, `Continent`, `Region`
-    IF @search_type = 'CountryCode' THEN
-        SET @sql_query = CONCAT(@sql_query, " WHERE country.Code='", @search_keyword, "'");
-	ELSEIF @search_type = 'Continent' THEN
-        SET @sql_query = CONCAT(@sql_query, " WHERE country.Continent='", @search_keyword, "'");
-	ELSEIF @search_type = 'Region' THEN
-        SET @sql_query = CONCAT(@sql_query, " WHERE country.Region='", @search_keyword, "'");
+    -- WHERE - Search Tabel  `country` 
+    --         Search Column `CountryCode`, `Continent`, `Region`
+    IF in_search_type_enum = 'CountryCode' THEN
+        SET @sql_query = CONCAT(@sql_query, " WHERE country.Code='", in_search_keyword_var, "'");
+	ELSEIF in_search_type_enum = 'Continent' THEN
+        SET @sql_query = CONCAT(@sql_query, " WHERE country.Continent='", in_search_keyword_var, "'");
+	ELSEIF in_search_type_enum = 'Region' THEN
+        SET @sql_query = CONCAT(@sql_query, " WHERE country.Region='", in_search_keyword_var, "'");
     END IF;
 
-    # ORDER BY - Order Column city.`ID`
-    IF @order_direction IS NULL THEN
+    -- ORDER BY - Order Column city.`ID`
+    IF in_order_enum IS NULL THEN
         SET @sql_query = CONCAT(@sql_query, " ORDER BY city.ID ASC");
     ELSE 
-        SET @sql_query = CONCAT(@sql_query, " ORDER BY city.ID ", @order_direction);
+        SET @sql_query = CONCAT(@sql_query, " ORDER BY city.ID ", in_order_enum);
     END IF;
 
-    # LIMIT/OFFSET
-    SET @sql_query = CONCAT(@sql_query, " LIMIT ", @limit ," OFFSET ", @offset);
+    -- LIMIT/OFFSET - DEFAULT 10 / 0
+    SET @sql_query = CONCAT(@sql_query, " LIMIT ", IFNULL(in_limit_int, 10) ," OFFSET ", IFNULL(in_offset_int, 0));
 
 	PREPARE stmt FROM @sql_query;
 	EXECUTE stmt;
