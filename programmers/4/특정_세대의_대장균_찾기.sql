0be1f5121d816c8147d5489a6f634516a98604c1
devbyul <devbyul@gmail.com>
2024-07-02 17:22:27 +0900

    SELECT

---
+++ programmers/4/특정_세대의_대장균_찾기.sql
@@ -0,0 +1,20 @@
+-- https://im-codding.tistory.com/82#google_vignette
+WITH RECURSIVE GEN_DATA AS (
+    SELECT
+        ID,
+        PARENT_ID,
+        1 AS GEN
+    FROM ECOLI_DATA 
+    WHERE PARENT_ID IS NULL
+    UNION ALL
+    SELECT
+        ECOLI_DATA.ID,
+        ECOLI_DATA.PARENT_ID,
+        (GEN + 1) AS GEN
+    FROM ECOLI_DATA
+        JOIN GEN_DATA ON ECOLI_DATA.PARENT_ID = GEN_DATA.ID
+)
+SELECT 
+    ID
+FROM GEN_DATA 
+WHERE GEN = 3
